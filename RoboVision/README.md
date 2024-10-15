fb588860cf9c48101b0adc5d3126d1a1edeac83e
justynaprzyborska <justyna.przyborska123@gmail.com>
2024-10-15 22:01:28 +0100

    First commit!

---
+++ RoboVision/README.md
@@ -0,0 +1,4 @@
+## RoboVision
+
+There is no established project structure yet so I'll be placing any miscellaneous scripts to with computer vision here - Justyna
+
---
+++ RoboVision/SeeThePlate.py
@@ -0,0 +1,7 @@
+"""
+    Detects weight plates and their holes in an image, 
+    drawing two bounding ellipses around each to outline both 
+    the outer circumference and the inner hole.
+
+"""
+
